e8df88e7f4113de5ba5cfc4551818b5fb329c04f
ppedra <asb@asd.com>
2018-05-24 20:33:22 -0300

    basic scene management

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+	public Animator animator;
+	public int sceneToLoadIndex;
+	
+	void Update () {
+		if (Input.GetKeyDown("p")){
+			FadeToLevel(1);
+		}
+	}
+	
+
+	public void FadeToLevel(int sceneIndex){
+		sceneToLoadIndex = sceneIndex;
+		animator.SetTrigger("FadeOutAnim");
+	}
+
+	public void OnFadeComplete(){
+		SceneManager.LoadScene(sceneToLoadIndex);
+	}
+
+}
